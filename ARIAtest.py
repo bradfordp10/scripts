77e61ccae5321b69efe33d7737d12a18efd9d66f
bradfordp10 <60279410+bradfordp10@users.noreply.github.com>
2022-06-13 09:18:31 -0700

    Add files via upload

--- ARIAtest.py
+++ ARIAtest.py
@@ -4,21 +4,21 @@ import time
 from io import StringIO
 
 s = speedtest.Speedtest()
-header = pd.read_csv(StringIO(s.results.csv_header()), sep=',')
+s.get_best_server()
+header = pd.read_csv(StringIO(s.results.csv_header() + ",Test Timestamp"), sep=',')
 print(header)
 header.to_csv('ARIAtest.csv', mode='a', index=False)
 
 while True:
     try:
-        s = speedtest.Speedtest()
-        s.get_best_server()
+        speedtest.Speedtest()
         s.download()
         s.upload()
-        data = pd.read_csv(StringIO(s.results.csv()), sep=',')
+        data = pd.read_csv(StringIO(s.results.csv() + ",{date}".format(date=pd.Timestamp.now())), sep=',')
         print(data)
         data.to_csv('ARIAtest.csv', mode='a', index=False)
     except Exception as exc:
-        print("[!!!] {err} {date}".format(err=exc,date=pd.Timestamp.now().isoformat()))
-        error = pd.read_csv(StringIO(",,,{date}Z,,,,,,".format(date=pd.Timestamp.now().isoformat())))
+        print("[!!!] {err} {date}".format(err=exc,date=pd.Timestamp.now()))
+        error = pd.read_csv(StringIO(",,,,,,,,,,{date}".format(date=pd.Timestamp.now())))
         error.to_csv('ARIAtest.csv', mode='a', index=False)
         time.sleep(15)
